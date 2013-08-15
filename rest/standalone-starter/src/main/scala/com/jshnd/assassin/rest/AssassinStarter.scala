91c47d16c2bedbb8e0a33480ae5c70c2a05b9b20
Jon Sabados <jon.sabados@gmail.com>
2013-08-14 19:49:38 -0600

    Birth of a runnable war

---
+++ rest/standalone-starter/src/main/scala/com/jshnd/assassin/rest/AssassinStarter.scala
@@ -0,0 +1,28 @@
+package com.jshnd.assassin.rest
+
+import org.mortbay.jetty.Server
+import org.mortbay.jetty.bio.SocketConnector
+import org.mortbay.jetty.webapp.WebAppContext
+
+object AssassinStarter extends App {
+  val server = new Server()
+  val connector = new SocketConnector()
+
+  // TODO - make this command line driven
+  connector.setPort(8080)
+  server.setConnectors(Array(connector))
+
+  val context = new WebAppContext()
+  context.setServer(server)
+  context.setContextPath("/")
+
+  val protectionDomain = AssassinStarter.getClass.getProtectionDomain
+  val location = protectionDomain.getCodeSource.getLocation
+  context.setWar(location.toExternalForm)
+
+  server.addHandler(context)
+  server.start()
+  System.in.read()
+  server.stop()
+  server.join()
+}
