3a10e1031a4a15e7fcd531f13a7d4af06d1790aa
Jon Sabados <jon.sabados@gmail.com>
2013-08-18 21:45:28 -0600

    Fixed some copy and paste

--- rest/rest-core/src/test/java/com/jshnd/assassin/rest/AssassinAuthenticationSourceTest.scala
+++ rest/rest-core/src/test/java/com/jshnd/assassin/rest/AssassinAuthenticationSourceTest.scala
@@ -14,31 +14,11 @@ class AssassinAuthenticationSourceTest extends FunSpec {
     val token = new UsernamePasswordToken()
     token.setUsername("test@example.com")
 
-    it("Should return a user with an invalid hash when no user found") {
-      val result = new AssassinAuthenticationSource((String) => List()).authenticationInfo(token).asInstanceOf[SimpleAuthenticationInfo]
-      assert(result.getCredentials === AssassinAuthenticationSource.WONT_MATCH_HASH)
-      assert(result.getPrincipals.asList().get(0) === User(None, "test@example.com", "Nope", None, AssassinAuthenticationSource.WONT_MATCH_HASH))
-    }
+    val validUser = new User(Some(1), "test@example.com", "test", Some("Test User"), "12345")
 
-    it("Should return a user when found") {
-      val found = new User(Some(1), "test@example.com", "test", Some("Test User"), "12345")
-      def testIt(x: String): List[User] = {
-        if(!x.equals("test@example.com")) fail("Wrong username passed")
-        List(found)
-      }
-      val result = new AssassinAuthenticationSource(testIt).authenticationInfo(token).asInstanceOf[SimpleAuthenticationInfo]
-      assert(result.getCredentials === "12345")
-      assert(result.getPrincipals.asList().get(0) === found)
-    }
-
-    it("Should use the username as the salt") {
-      val found = new User(Some(1), "test@example.com", "test", Some("Test User"), "12345")
-      def testIt(x: String): List[User] = {
-        if(!x.equals("test@example.com")) fail("Wrong username passed")
-        List(found)
-      }
-      val result = new AssassinAuthenticationSource(testIt).authenticationInfo(token).asInstanceOf[SimpleAuthenticationInfo]
-      assert(result.getCredentialsSalt.getBytes === "test@example.com".getBytes)
+    def happyPathFind(x: String): List[User] = {
+      if (!x.equals("test@example.com")) fail("Wrong username passed")
+      List(validUser)
     }
 
     it("Should blow up when more than one user is found") {
@@ -46,6 +26,23 @@ class AssassinAuthenticationSourceTest extends FunSpec {
       intercept[IllegalStateException] {
         new AssassinAuthenticationSource((x) => List(found, found)).authenticationInfo(token)
       }
+    }
+
+    it("Should return a user with an invalid hash when no user found") {
+      val result = new AssassinAuthenticationSource((String) => List()).authenticationInfo(token).asInstanceOf[SimpleAuthenticationInfo]
+      assert(result.getCredentials === AssassinAuthenticationSource.WONT_MATCH_HASH)
+      assert(result.getPrincipals.asList().get(0) === User(None, "test@example.com", "Nope", None, AssassinAuthenticationSource.WONT_MATCH_HASH))
+    }
+
+    it("Should return a user when found") {
+      val result = new AssassinAuthenticationSource(happyPathFind).authenticationInfo(token).asInstanceOf[SimpleAuthenticationInfo]
+      assert(result.getCredentials === "12345")
+      assert(result.getPrincipals.asList().get(0) === validUser)
+    }
+
+    it("Should use the username as the salt") {
+      val result = new AssassinAuthenticationSource(happyPathFind).authenticationInfo(token).asInstanceOf[SimpleAuthenticationInfo]
+      assert(result.getCredentialsSalt.getBytes === "test@example.com".getBytes)
     }
 
   }
