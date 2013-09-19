126bf578b493a7c2a4fe2e8c747cd5afe28f000b
Jon Sabados <jon.sabados@gmail.com>
2013-09-18 22:19:58 -0600

    Adding error classes to inputs when they are in error - part of #6

--- rest/webapp/src/main/webapp/js/controllers/enlistController.js
+++ rest/webapp/src/main/webapp/js/controllers/enlistController.js
@@ -2,13 +2,24 @@ Assassin.EnlistController = Ember.Controller.extend(AssassinSubmitter, {
     needs: ["login"],
     termsAccepted: false,
     passwordConfirm: "",
-    hasFieldFailures: function() {
-        return this.get("errors").fieldFailures.length > 0;
-    }.property("errors"),
-    hasGeneralFailures: function() {
-        return this.get("errors").generalFailures.length > 0;
-    }.property("errors"),
     errors: Errors.create({}),
+
+    hasFailures: function() {
+        return this.get("errors").generalFailures.length > 0 || this.get("errors").fieldFailures.length > 0;
+    }.property("errors"),
+
+    // TODO - there has got to be a better way to do this other than a slew of hasFailureFEILD functions
+    hasFailure: function(field) {
+        return this.get("errors").fieldFailures.any(function(error) {
+            return error.field == field;
+        });
+    },
+
+    hasFailureEmailAddress: function() {return this.hasFailure("emailAddress")}.property("errors"),
+    hasFailureHandle: function() {return this.hasFailure("handle")}.property("errors"),
+    hasFailureFullName: function() {return this.hasFailure("fullName")}.property("errors"),
+    hasFailurePassword: function() {return this.hasFailure("password")}.property("errors"),
+    hasFailurePasswordConfirm: function() {return this.hasFailure("passwordConfirm")}.property("errors"),
 
     requiredFieldsSet: function() {
         var model = this.get("model");
