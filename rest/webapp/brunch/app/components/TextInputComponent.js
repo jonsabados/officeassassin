5623fc477988ed6272d3c20d53256ec754da9fb7
Jon Sabados <jon.sabados@gmail.com>
2013-10-14 23:15:29 -0600

    Tweeking UI validation

---
+++ rest/webapp/brunch/app/components/TextInputComponent.js
@@ -0,0 +1,20 @@
+var Assassin = require("config/App");
+
+module.exports = Assassin.TextInputComponent = Ember.Component.extend({
+  classNames: ["form-group"],
+  classNameBindings: ["showError:has-error"],
+  hasFocusedOut: false,
+  errors: null,
+  invalid: Ember.computed.gt("errors.length", 0),
+  type: "text",
+  label: null,
+  inputId: null,
+  value: null,
+  placeholder: null,
+
+  showError: Ember.computed.and("invalid", "hasFocusedOut"),
+
+  focusOut: function () {
+    this.set("hasFocusedOut", true);
+  }
+});
